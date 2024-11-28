162bc2ee10fdb7df0cdddc1868b9ff24237e760c
Mr.Joker <162453434+AuroraDasiy@users.noreply.github.com>
2024-11-28 13:56:26 +0800

    Batch commit changes

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,10 +1,42 @@
-#include<stdio.h>
-#include<math.h>
+//#include<stdio.h>
+//#include<stdlib.h>
+//int compare(const void *x,const void*y){
+//	int intx=*((int*) x);int inty=*((int*)y);
+//	if(intx>inty){return 1;
+//	}
+//	else if(intx==inty){
+//		return 0;
+//	}else{return -1;
+//	}
+//}
+//int main(){
+//	 int arr[] = {5, 2, 9, 1, 5, 6};
+//	 qsort(arr,6,sizeof(int),compare);
+//	 for(int i=0;i<6;i++){
+//	 	printf("%d",arr[i]);
+//	 }
+//	
 
-int main(){
-	int h;int min;int h1,min1;
-//	scanf("%d %d",&h,&min);
-	h=1;min=20;h1=2;min1=30;
-	printf("%d",abs(h1));
+#include<stdio.h>
+#include<stdlib.h>
+int compare(const void* x,const void* y){
+	int xValue=*((int*)x);int yValue=*((int* )y);
+	if(xValue<yValue){
+		return -1;
+	}if(xValue==yValue){return 0;
+	}else{return 1;
+	}
 	
 }
+int main(){
+	int arr[5]={5,4,3,2,1};
+	qsort(arr,5,sizeof(int),compare);
+	for(int i=0;i<5;i++){
+		printf("%d",arr[i]);	}
+}
+
+
+
+
+
+
